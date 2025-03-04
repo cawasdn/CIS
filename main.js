3cbda19dc7cb4685be365722fd6bce102fab08eb
Daniel <cawasdn@gmail.com>
2025-03-04 09:20:00 +0800

    2nd

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 720,
+    height: 480,
+    frame: false,
+    resizable: false
   })
 
   win.loadFile('index.html')
