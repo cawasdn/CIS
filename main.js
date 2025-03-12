223d47d8d3fdb9539cf091cee833f87c05127f42
Daniel <cawasdn@gmail.com>
2025-03-12 16:48:02 +0800

    .

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 720,
     height: 480,
-    frame: false,
-    resizable: false
+    frame: true,
+    // resizable: false
   })
 
   win.loadFile('index.html')
